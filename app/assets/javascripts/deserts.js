eeb56f31aa13868893e605ce22e5d6710fb59ff0
nathanallen <thenathanator@gmail.com>
2016-01-12 10:05:00 -0800

    reminder to not code in js file

--- app/assets/javascripts/deserts.js
+++ app/assets/javascripts/deserts.js
@@ -1,3 +1,48 @@
+//
+// DO NOT CODE IN THIS FILE!
+//
+// WORK ONLY IN YOUR CHROME DEVELOPER CONSOLE
+//
+// DO NOT CODE IN THIS FILE!
+//
+
+/*
+ * Cookie Manipulation
+ * Source: http://www.quirksmode.org/js/cookies.html
+ */
+
+function createCookie(name,value,days) {
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime()+(days*24*60*60*1000));
+    var expires = "; expires="+date.toGMTString();
+  }
+  else var expires = "";
+  document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+    var c = ca[i];
+    while (c.charAt(0)==' ') c = c.substring(1,c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
+
+function eraseCookie(name) {
+  createCookie(name,"",-1);
+}
+
+
+
+
+/*
+ * Cookie Challenge Prompts
+ */
+
 $(function(){
   console.log("document.cookie =>", document.cookie)
   can_has = readCookie('can_has')
@@ -48,36 +93,3 @@ $(function(){
 
 })
 
-
-
-
-
-
-
-// Cookie Manipulation
-// Source: http://www.quirksmode.org/js/cookies.html
-
-function createCookie(name,value,days) {
-  if (days) {
-    var date = new Date();
-    date.setTime(date.getTime()+(days*24*60*60*1000));
-    var expires = "; expires="+date.toGMTString();
-  }
-  else var expires = "";
-  document.cookie = name+"="+value+expires+"; path=/";
-}
-
-function readCookie(name) {
-  var nameEQ = name + "=";
-  var ca = document.cookie.split(';');
-  for(var i=0;i < ca.length;i++) {
-    var c = ca[i];
-    while (c.charAt(0)==' ') c = c.substring(1,c.length);
-    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-  }
-  return null;
-}
-
-function eraseCookie(name) {
-  createCookie(name,"",-1);
-}
