9e9713bc8f855dd7763df459d1b9936522745223
nathanallen <thenathanator@gmail.com>
2016-01-12 15:52:28 -0800

    refactor to use helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def authorize
+    redirect_to root_path and return if not is_logged_in
+  end
+
+  def is_logged_in
+    cookies[:can_has] == "yarly"
+  end
 end
--- app/controllers/desserts_controller.rb
+++ app/controllers/desserts_controller.rb
@@ -1,5 +1,8 @@
 class DessertsController < ApplicationController
 
+  before_action :authorize, only: [:show, :favorite, :save_as_favorite]
+  before_action :set_favorite_dessert_id, only: [:show, :favorite]
+
   # GET /desserts
   def index
     @desserts = Dessert.all
@@ -7,16 +10,12 @@ class DessertsController < ApplicationController
 
   # GET /desserts/1
   def show
-    redirect_to root_path and return if not is_logged_in
-    @favorite_dessert_id = session[:favorite_dessert_id]
     @dessert = Dessert.find(params[:id])
   end
 
   # GET /desserts/favorite
   # Shows the user's favorite dessert (if set in the session)
   def favorite
-    redirect_to root_path and return if not is_logged_in
-    @favorite_dessert_id = session[:favorite_dessert_id] # Get favorite
     @dessert = Dessert.find(@favorite_dessert_id)
     render 'show'
   end
@@ -24,7 +23,6 @@ class DessertsController < ApplicationController
   # POST /desserts/1/save_as_favorite
   # Sets the user's favorite dessert in their session
   def save_as_favorite
-    redirect_to root_path and return if not is_logged_in
     session[:favorite_dessert_id] = params[:id] # SET favorite
     flash[:notice] = "dessert #{params[:id]} saved in session as favorite!"
     redirect_to root_path
@@ -32,8 +30,8 @@ class DessertsController < ApplicationController
 
   private
 
-  def is_logged_in
-    cookies[:can_has] == "yarly"
+  def set_favorite_dessert_id
+    @favorite_dessert_id = session[:favorite_dessert_id]
   end
 
 end
