4b0942132e475742f9af1e8bcb14945d51b23b05
nathanallen <thenathanator@gmail.com>
2016-01-12 15:42:54 -0800

    solution to challenges 3+4

--- app/controllers/desserts_controller.rb
+++ app/controllers/desserts_controller.rb
@@ -8,12 +8,7 @@ class DessertsController < ApplicationController
   # GET /desserts/1
   def show
     redirect_to root_path and return if not is_logged_in
-
-    ## CHALLENGE 4: This is my favorite dessert!
-    ### Detect whether the dessert the user is viewing
-    ### is their favorite_dessert, and add the words
-    ### 'FAVORITE dessert' in the `views/desserts/show.html.erb` template
-
+    @favorite_dessert_id = session[:favorite_dessert_id]
     @dessert = Dessert.find(params[:id])
   end
 
@@ -21,13 +16,8 @@ class DessertsController < ApplicationController
   # Shows the user's favorite dessert (if set in the session)
   def favorite
     redirect_to root_path and return if not is_logged_in
-
-    ## CHALLENGE 3: Render my favorite_dessert!
-    ### Check my session for my `favorite_dessert_id`
-    ### and then render my favorite dessert
-    ### AS-IS this will raise an error
-    favorite_dessert_id = 34404 # CHANGE THIS
-    @dessert = Dessert.find(favorite_dessert_id)
+    @favorite_dessert_id = session[:favorite_dessert_id] # Get favorite
+    @dessert = Dessert.find(@favorite_dessert_id)
     render 'show'
   end
 
